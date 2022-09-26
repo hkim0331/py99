2056c035fdf51f9e9cb7ea57f8d434657fbaeb25
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-09-26 22:17:07 +0900

    prep release 0.40.0

--- src/clj/py99/routes/login.clj
+++ src/clj/py99/routes/login.clj
@@ -18,8 +18,7 @@
    note: parameter is a string. cf. (db/get-user {:login login})"
   [login]
   (let [ep (str l22 "/api/user/" login)
-        resp (hc/get ep {:as :json})] ;; {:as :json} failed in docker
-        ;;resp (hc/get ep)]
+        resp (hc/get ep {:as :json})]
     (log/info "login" (get-in resp [:body :login]))
     ;; (log/debug "(:body resp)" (:body resp))
     (:body resp)))

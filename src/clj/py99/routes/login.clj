07510885b5987c6e43d39ec7fe3bdf8b1e1b01e8
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-02-19 10:27:00 +0900

    release 0.62.0

--- src/clj/py99/routes/login.clj
+++ src/clj/py99/routes/login.clj
@@ -10,7 +10,7 @@
    [struct.core :as st]
    #_[py99.db.core :as db]))
 
-(def ^:private version "0.61.0")
+(def ^:private version "0.62.0")
 
 (def ^:private l22 "https://l22.melt.kyutech.ac.jp")
 

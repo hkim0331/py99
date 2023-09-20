4c5b2889891d1aaf69394e4f8cfdda04fd21bd53
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-09-20 22:54:04 +0900

    indent is a bad idea

--- src/clj/py99/routes/admin.clj
+++ src/clj/py99/routes/admin.clj
@@ -3,24 +3,13 @@
    #_[clojure.edn :as edn]
    [clojure.java.io :as io]
    clojure.pprint
+   [clojure.string :as str]
    #_[clojure.string :refer [split-lines starts-with? replace-first]]
    [py99.db.core :as db]
    [py99.layout :as layout]
    [py99.middleware :as middleware]
-   [ring.util.response :refer [redirect]]))
-
-;; (defn seed-problems-from-markdown!
-;;   "rebuild problems table from docs/problems.md"
-;;   [request]
-;;   (let [problems (->> (io/resource "docs/problems.md")
-;;                       slurp
-;;                       split-lines
-;;                       (filter #(starts-with? % "1. "))
-;;                       (map #(replace-first % #"1. " "")))]
-;;     (db/delete-problems-all!)
-;;     (doseq [[i line] (map-indexed #(vector (inc %1) %2) problems)]
-;;       (db/create-problem! {:num i :problem line}))
-;;     (layout/render request "home.html" {:docs "done seed problems."})))
+   [ring.util.response :refer [redirect]]
+   [clojure.string :as s]))
 
 (defn seed-problems!
   "rebuild problems table from resources/docs/problems.edn.
@@ -34,15 +23,36 @@
       (db/create-problem! {:num i :problem (:problem m) :test (:test m)}))
     (layout/render request "home.html" {:docs "seeded problems."})))
 
-;; FIXME: pretty print
+(defn- p1
+  [s]
+  (-> s
+      (str/replace #"\r" "")
+      (str/replace #"\n\s+" " ")))
+
+(defn- p2
+  [s]
+  (if s
+    (str/replace s #"\r\n" "")
+    ""))
+
+(defn- pretty-print
+  [col]
+  (clojure.pprint/pprint
+   (for [{:keys [problem test]} col]
+     {:problem (p1 problem)
+      :test (p2 test)})))
+
+;; FIXME: lazy seq
 (defn dump-problems!
   "output problems to resources/docs/problems-dump.edn"
   [_]
-  (spit (io/resource "docs/problems-dump.edn")
-        (with-out-str (clojure.pprint/pprint (db/fetch-problems))))
-  {:status 200
-   :headers {"content-type" "text/html"}
-   :body "dumped to resouces/docs/problems-dump.edn"})
+  (let [problems (db/fetch-problems)]
+    (def it problems)
+    (spit (io/resource "docs/problems-dump.edn")
+          (with-out-str (pretty-print problems)))
+    {:status 200
+     :headers {"content-type" "text/html"}
+     :body "dumped to resouces/docs/problems-dump.edn"}))
 
 (defn admin-page [request]
   (layout/render request "admin.html"))

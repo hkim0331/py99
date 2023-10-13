84a68878b3a2bfd6f9e1744a150ef19dd340e3b6
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-10-13 14:13:10 +0900

    0.71.9 #include(space)num

--- src/clj/py99/routes/login.clj
+++ src/clj/py99/routes/login.clj
@@ -12,8 +12,8 @@
    #_[struct.core :as st]
    #_[py99.db.core :as db]))
 
-(def ^:private version "0.71.7.1")
-(def ^:private updated "2023-10-08 23:05:00")
+(def ^:private version "0.71.9")
+(def ^:private updated "2023-10-13 14:12:53")
 
 (def ^:private l22 "https://l22.melt.kyutech.ac.jp")
 

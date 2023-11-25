81e4e33ff7a92bfcda7f05d9bcb12e6acf358d0e
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-11-25 16:15:29 +0900

    release 0.76.0 for midterm exam

--- src/clj/py99/routes/login.clj
+++ src/clj/py99/routes/login.clj
@@ -12,8 +12,8 @@
    #_[struct.core :as st]
    #_[py99.db.core :as db]))
 
-(def ^:private version "0.75.0")
-(def ^:private updated "2023-11-24 14:31:06")
+(def ^:private version "0.76.0")
+(def ^:private updated "2023-11-25 16:15:11")
 
 (def ^:private l22 "https://l22.melt.kyutech.ac.jp")
 

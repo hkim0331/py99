f2796010683a4532f364942a397a6540f2d2f85c
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-12-21 13:47:53 +0900

    0.80.0

--- src/clj/py99/routes/login.clj
+++ src/clj/py99/routes/login.clj
@@ -12,8 +12,8 @@
    #_[struct.core :as st]
    #_[py99.db.core :as db]))
 
-(def ^:private version "0.79.2")
-(def ^:private updated "2023-12-17 10:36:07")
+(def ^:private version "0.80.0")
+(def ^:private updated "2023-12-21 13:47:43")
 
 (def ^:private l22 "https://l22.melt.kyutech.ac.jp")
 

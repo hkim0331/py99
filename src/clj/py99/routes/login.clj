ab732711ef17cd03203dc425804c3b6f6343dfe5
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-03-06 15:09:24 +0900

    updated CHANGELOG.md

--- src/clj/py99/routes/login.clj
+++ src/clj/py99/routes/login.clj
@@ -12,8 +12,8 @@
    #_[struct.core :as st]
    #_[py99.db.core :as db]))
 
-(def ^:private version "0.92.883")
-(def ^:private updated "2024-02-23 12:24:40")
+(def ^:private version "0.93.888")
+(def ^:private updated "2024-03-06 15:08:52")
 
 (def ^:private l22 "https://l22.melt.kyutech.ac.jp")
 

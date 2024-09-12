df37f2ea3f78b28cd77e9d7a818f30728318da06
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-09-12 11:28:41 +0900

    use ruff

--- src/clj/py99/routes/login.clj
+++ src/clj/py99/routes/login.clj
@@ -12,8 +12,8 @@
    #_[struct.core :as st]
    #_[py99.db.core :as db]))
 
-(def ^:private version "v1.1-SNAPSHOT")
-(def ^:private updated "2024-09-11 15:29:29")
+(def ^:private version "v1.1.927")
+(def ^:private updated "2024-09-12 11:28:30")
 
 (def ^:private l22 "https://l22.melt.kyutech.ac.jp")
 

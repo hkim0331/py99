cde7cc548f893d6ea087c57f85dadc9e417591f3
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2025-01-10 09:06:00 +0900

    ruff 0.9.0

--- src/clj/py99/routes/login.clj
+++ src/clj/py99/routes/login.clj
@@ -10,8 +10,8 @@
    [py99.middleware :as middleware]
    [ring.util.response :refer [redirect]]))
 
-(def ^:private version "v1.25-SNAPSHOT")
-(def ^:private updated "2025-01-08 23:16:29")
+(def ^:private version "v1.25.1262")
+(def ^:private updated "2025-01-10 09:05:47")
 
 (def ^:private l22 "https://l22.melt.kyutech.ac.jp")
 
